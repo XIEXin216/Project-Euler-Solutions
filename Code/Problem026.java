757c467330575830a281fe09c26197cf4645c5e8
XIEXin216 <43174468+XIEXin216@users.noreply.github.com>
2020-04-12 23:02:44 +0800

    Update Problem026.java

--- Code/Problem026.java
+++ Code/Problem026.java
@@ -1,7 +1,9 @@
 package ProjectEuler;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Project Euler problem 26
@@ -14,6 +16,10 @@ public final class Problem026 {
 	private static final int PARA = 1000;
 	
 	public static void main(String[] args) {
+		for(int i = 2; i<= 10; i++) {
+			System.out.println(i + "-->" + cycle(i));
+		}
+		
 		run();
 	}
 	
@@ -21,30 +27,46 @@ public final class Problem026 {
 		int max = 0;
 		for(int i = 2; i< PARA; i++) {
 			max = max > cycle(i) ? max : cycle(i);
+//			if(max == 884)
+//				System.out.println(i);
 		}
+		
 		System.out.println(max);
 	}
 	
-	// a refers to numerator
-	// b refers to denominator
-	// c refers to remainder
+	/*
+	 * a refers to numerator
+	 * b refers to denominator
+	 * c refers to remainder
+	 */
 	private static int cycle(int b) {
 		//initionalization
 		int c = 1;
-		Integer cnt = 0;
+		Integer cnt = 1;
 		List<Integer> re = new ArrayList<Integer>();
+		
+		while(c < b) {
+			c *= 10;
+		}
+		
 		while(c != 0) {
 			while(c < b) {
 				c *= 10;
+				cnt++;
 			}
+			
 			c = c%b;
-			if(re.contains(c) || c == 0) {
+			
+			if(re.contains(c)) {
 				break;
 			}
+			else if( c == 0)
+				cnt = 0;
 			else {
 				re.add(c);
 				cnt++;
 			}
+			
 		}
 		
 		return cnt;
